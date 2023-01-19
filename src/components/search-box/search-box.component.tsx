e61c694f372448adf2035af7d7f0c78e252a3506
Juan Pablo Lagos Moreno <juan.lagos.moreno91@gmail.com>
2023-01-19 20:02:18 -0300

    comenzando primeras funcionalidades con typescript

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,22 @@
+import './search-box.styles.css';
+
+interface ISearchBoxProps {
+    className: string;
+    placeholder?: string;
+
+}
+
+interface ISearchBoxProps {
+  onChangeHandler: (a: string) => void
+}
+
+const SearchBox = ({ className, placeholder, onChangeHandler }: ISearchBoxProps ) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={(e) => onChangeHandler(e)}
+  />
+);
+
+export default SearchBox;
